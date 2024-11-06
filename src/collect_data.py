149ff976fb520404a402c801500c42240ce4cc34
Francisco Castillo <122114861+FranzCastillo@users.noreply.github.com>
2024-11-05 23:41:01 -0600

    CSV Filename as Argument

--- src/collect_data.py
+++ src/collect_data.py
@@ -6,7 +6,7 @@ from utils.webcam import Webcam
 from utils.hand_processor import HandProcessor
 
 DATA_DIR = 'data'
-PICTURES_PER_CLASS = 75
+PICTURES_PER_CLASS = 100
 WIN_NAME = 'Collecting ASL Data'
 
 
